6d23585b781042ef5898434738723c7e0c46397c
Eduard Zhaloba <zhalobaeduard02@gmail.com>
2026-05-09 16:53:47 +0300

    new third task

--- lab_10/lab_10.cpp
+++ lab_10/lab_10.cpp
@@ -5,26 +5,36 @@ int main()
 {
     int a1 = 5, a2 = 7, a3 = 10, counter = 0;
     
-    std::thread t1([&counter](int thread_id = 1) {
-    for (int i = 0; i < 10000; ++i) {
-        counter += thread_id;
-        
+    std::mutex mut;
+    std::thread t1([&counter, &mut](int thread_id = 1) {
+        for (int i = 0; i < 10; ++i) {
+            mut.lock();
+            counter += thread_id;
+            mut.unlock();
+        }
+        });
+  
+        std::thread t2([&counter, &mut](int thread_id = 2) {
+            for (int i = 0; i < 10; ++i) {
+                mut.lock();
+                counter += thread_id;
+             
+             
+                mut.unlock();
+            }
+        });
+    
+        std::thread t3([&counter, &mut](int thread_id = 3) {
+            for (int i = 0; i < 10; ++i) {
+                mut.lock();
+                counter += thread_id;
+              
+              
+                mut.unlock();
+            }
+        });
+        t1.join();
+        t2.join();
+        t3.join();
+		std::cout << "Final counter value: " << counter << std::endl;
     }
-    });
-    std::thread t2([&counter](int thread_id = 2) {
-    for (int i = 0; i < 10000; ++i) {
-        counter += thread_id;
-        
-    }
-    });
-    std::thread t3([&counter](int thread_id = 3) {
-    for (int i = 0; i < 10000; ++i) {
-        counter += thread_id;
-        
-    }
-    });
-    t1.join();
-    t2.join();
-    t3.join();
-    std::cout << "Counter after joining threads: " << counter << std::endl;
-}
