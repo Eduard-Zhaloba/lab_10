05e8509c752c9b7c72f76f900432fbfacc79e1fc
Eduard Zhaloba <zhalobaeduard02@gmail.com>
2026-05-09 14:15:16 +0300

    second task complete

--- lab_10/lab_10.cpp
+++ lab_10/lab_10.cpp
@@ -3,10 +3,25 @@
 
 int main()
 {
-	int a1 = 5, a2 = 7, a3 = 10;
-    std::thread t1([a1]() {std::cout << "Tread1: " << a1 * a1 << std::endl; });
-    std::thread t2([a2]() {std::cout << "Tread2: " << a2 * a2 << std::endl; });
-    std::thread t3([a3]() {std::cout<<"Tread3: "<<a3*a3<<std::endl;});
+	int a1 = 5, a2 = 7, a3 = 10, counter = 0;
+    std::thread t1([&counter](int thread_id=1) {
+        for (int i = 0; i < 10; ++i) { 
+            counter += thread_id;
+            std::cout << "Thread " << thread_id << " set counter to: " << counter << std::endl;
+        }
+    });
+    std::thread t2([&counter](int thread_id = 2) {
+        for (int i = 0; i < 10; ++i) {
+            counter += thread_id;
+            std::cout << "Thread " << thread_id << " set counter to: " << counter << std::endl;
+        }
+    });
+    std::thread t3([&counter](int thread_id = 3) {
+        for (int i = 0; i < 10; ++i) {
+            counter += thread_id;
+            std::cout << "Thread " << thread_id << " set counter to: " << counter << std::endl;
+        }
+		});
     t1.join();
     t2.join();
     t3.join();
