e148fd575288f03bb063e34ad52e061798fa5fb7
Eduard Zhaloba <zhalobaeduard02@gmail.com>
2026-05-10 00:46:18 +0300

    task 5 complete

--- lab_10/lab_10.cpp
+++ lab_10/lab_10.cpp
@@ -2,6 +2,53 @@
 #include <thread>
 #include <mutex>  
 #include <chrono>
+#include <condition_variable>
+#include <queue>
+
+class SafeQueue {
+private:
+    std::queue<int> q;
+    std::mutex mut3;
+    std::condition_variable producer;
+    std::condition_variable consumer;
+public:
+    void enqueue(int value) {
+        std::unique_lock<std::mutex> lock(mut3);
+
+        producer.wait(lock, [this]() { return q.size()<5; });
+
+        q.push(value);
+        std::cout << "Produced: " << value << " (Size: " << q.size() << ")" << std::endl;
+        consumer.notify_one();
+    }
+
+    int dequeue() {
+        std::unique_lock<std::mutex> lock(mut3);
+        consumer.wait(lock, [this]() { return !q.empty(); });
+        int value = q.front();
+        q.pop();
+        std::cout << "Consumed: " << value << " (Size: " << q.size() << ")" << std::endl;
+
+        producer.notify_one();
+        return value;
+    }
+};
+
+void producer_task(SafeQueue& sq) {
+    for (int i = 1; i <= 15; ++i) {
+        sq.enqueue(i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+    }
+}
+
+void consumer_task(SafeQueue& sq) {
+    for (int i = 1; i <= 15; ++i) {
+        sq.dequeue();
+        std::this_thread::sleep_for(std::chrono::milliseconds(300)); 
+    }
+};
+/// /////////////////////////////////////////////////////////
+
 std::mutex mut1;
 std::mutex mut2;
 void thread_A() {
@@ -75,5 +122,12 @@ int main()
         std::thread t5(thread_B);
         t4.join();
         t5.join();
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        SafeQueue sq;
+        std::thread t6(producer_task, std::ref(sq));
+        std::thread t7(consumer_task, std::ref(sq));
+
+        t6.join();
+        t7.join();
 		return 0;
     }
